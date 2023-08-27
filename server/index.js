41d5446fe12f74321625a224ead3e2647ec4f03c
jfi1000 <jf.izaguirre@gmail.com>
2023-08-27 10:58:56 -0600

    ya inserta en la bd

--- server/index.js
+++ server/index.js
@@ -2,9 +2,46 @@ const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
 const PORT = 3001;
+//código jf
+const { Country } = require('./src/db.js');
+const cargarPaises = async () => {
+  // Cargar países desde la API y guardar en la base de datos
+  const URL_API = 'http://localhost:5000/countries';
+  try {
+    const response = await axios.get(URL_API); // Cambia esto con la URL real
+    const countriesData = response.data;
 
-conn.sync({ force: true }).then(() => {
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-})
-}).catch(error => console.error(error))
+    const paisesPromises = countriesData.map(async (paisData) => {
+      const nuevoPais = {
+        id: paisData.cca3,
+        name: paisData.name.common,
+        flagImage: paisData.flags.png,
+        // continent: paisData.continents[0],
+        continent: paisData.continents && paisData.continents[0],
+        // capital: paisData.capital[0],
+        capital: paisData.capital && paisData.capital[0] ? paisData.capital[0] : 'Unknown', // Usar 'Unknown' si es null
+        subregion: paisData.subregion,
+        area: paisData.area,
+        population: paisData.population
+      };
+      await Country.create(nuevoPais);
+      
+    });
+    
+    await Promise.all(paisesPromises); // Espera a que todas las inserciones se completen
+
+    console.log('Datos cargados exitosamente en la base de datos');
+  } catch (error) {
+    console.error('Error al cargar los datos:', error);
+  }
+};
+
+
+  conn.sync({ force: true }).then(async () => {
+
+    await cargarPaises(); // Cargar los países al levantar el servidor
+
+    server.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    })
+    }).catch(error => console.error(error))
--- server/src/models/Country.js
+++ server/src/models/Country.js
@@ -3,10 +3,40 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
+  // const Country = sequelize.define('Country', {
+
   sequelize.define('Country', {
+    id: {
+      type: DataTypes.STRING(3),
+      primaryKey: true,
+      allowNull: false,
+    },  
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    flagImage: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    continent: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subregion: {
+      type: DataTypes.STRING,
+    },
+    area: {
+      type: DataTypes.FLOAT,
+    },
+    population: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
   });
 };
